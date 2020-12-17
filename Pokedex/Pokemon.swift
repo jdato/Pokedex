37aac387535fb9ec2d273e2a8f7438e6f0a738a4
Johannes Dato <j.dato@dr-flex.de>
2020-12-17 14:01:24 +0100

    Initial commit

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Johannes Dato on 17.12.20.
+//
+
+import Foundation
+
+struct Pokemon {
+    let name: String
+    let number: Int
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let pokemon = [
+        Pokemon(name: "Glumanda", number: 1),
+        Pokemon(name: "Bisasam", number: 2),
+        Pokemon(name: "Schiggy", number: 3)
+    ]
+    
+    // How many sections do we have?
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    // How many rows has our section?
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    
+    // Fill the table wit each cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
+        cell.textLabel?.text = pokemon[indexPath.row].name
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PokemonSegue" {
+            // If segue.destination can be cast to PokemonViewController go in
+            if let destination = segue.destination as? PokemonViewController {
+                destination.pokemon = pokemon[tableView.indexPathForSelectedRow!.row]
+            }
+        }
+    }
 }
 
